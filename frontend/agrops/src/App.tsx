14102f2464c0e9f89f1d4b2037d49750acde57dd
SamLorenzoSanc <68753976+SamLorenzoSanc@users.noreply.github.com>
2026-07-10 00:04:51 +0100

    API REST to Message

---
+++ frontend/agrops/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function App() {
+
+  return (
+    <div>
+      <h1>Plataforma de gestión de operaciones</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App
